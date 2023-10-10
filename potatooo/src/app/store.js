697b71705297f8785a6df426faf3c4a66894fea0
Nandhiniboopalan <118872477+Nandhiniboopalan@users.noreply.github.com>
2023-10-10 23:34:19 +0530

    Delete potatooo directory

--- potatooo/src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import donorsReducer from "../features/usersSlice";
-export const store = configureStore({
-  reducer: {
-    donors: donorsReducer,
-  },
-});
-
-export default store;
